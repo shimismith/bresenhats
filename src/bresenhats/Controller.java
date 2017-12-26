fdf8c051c41ff5b6e683e9088fab5e6c67e1bd09
shimismith <smith.shimi@gmail.com>
2017-12-26 13:26:16 -0500

    Cleaned up controller code a bit

--- src/bresenhats/Controller.java
+++ src/bresenhats/Controller.java
@@ -1,49 +1,50 @@
 package bresenhats;
 
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
 public class Controller {
 
   /** The player this controller controls */
-  private MovableGameObject player;
+  private Player player;
 
-  public Controller(MovableGameObject player) {
+  public Controller(Player player) {
     this.player = player;
   }
 
   /** Handles key inputs */
   public void handleKeyPress(KeyEvent e) {
-    switch (e.getCode()) {
-      case LEFT:
-        this.player.getVel().setX(-10);        
-        break;
-      case RIGHT:
-        this.player.getVel().setX(10);
-        break;
-      case UP:
-        // only can jump when on ground
-        if (this.player.isOnGround()) {
-          this.player.getVel().setY(-20);
-          this.player.setOnGround(false);
-        }
-        break;
+    KeyCode code = e.getCode();
+    if (code == KeyCode.LEFT) {
+      this.player.getVel().setX(-10);
+    } 
+    else if (code == KeyCode.RIGHT) {
+      this.player.getVel().setX(10);
+    }
+    else if (code == KeyCode.UP) {
+      // only can jump when on ground
+      if (this.player.isOnGround()) {
+        this.player.getVel().setY(-20);
+        this.player.setOnGround(false);
+      }
     }
   }
 
   /** Handles key inputs */
   public void handleKeyRelease(KeyEvent e) {
-    switch (e.getCode()) {
-      case LEFT:
-        // if going left - could be going right but left button was being held then released
-        if (this.player.getVel().getX() < 0) {
-          this.player.getVel().setX(0);
-        }
-        break;
-      case RIGHT:
-        if (this.player.getVel().getX() > 0) {
-          this.player.getVel().setX(0);
-        }
-        break;
+
+    KeyCode code = e.getCode();
+
+    if (code == KeyCode.LEFT) {      
+      // if going left - could be going right but left button was being held then released
+      if (this.player.getVel().getX() < 0) {
+        this.player.getVel().setX(0);
+      }
+    } 
+    else if (code == KeyCode.RIGHT) {      
+      if (this.player.getVel().getX() > 0) {
+        this.player.getVel().setX(0);
+      }
     }
   }
 }
