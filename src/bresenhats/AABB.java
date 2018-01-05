e9156d87e1da507c852ad5fa215b58ecf6c26367
russellJacob <1bruss072@gmail.com>
2018-01-04 20:29:26 -0500

    Added Level and AABB classes

---
+++ src/bresenhats/AABB.java
@@ -0,0 +1,30 @@
+package bresenhats;
+
+public class AABB {
+    
+    //Defines a rectangle
+    Vector2D topLeft;
+    Vector2D bottomRight;
+    
+    public AABB(int topLeftX, int topLeftY, int bottomRightX, int bottomRightY){
+        topLeft.setX(topLeftX);
+        topLeft.setY(topLeftY);
+        bottomRight.setX(bottomRightX);
+        bottomRight.setY(bottomRightY);
+    }
+    
+    //Returns true if bounding boxes have intersected
+    boolean hasIntersected(AABB secondBoundingBox){
+        
+        //Check if boxes are seperate
+        if(this.bottomRight.getX() < secondBoundingBox.topLeft.getX() || this.topLeft.getX() > secondBoundingBox.bottomRight.getX())
+            return false;
+        if(this.bottomRight.getY() < secondBoundingBox.topLeft.getY() || this.topLeft.getY() > secondBoundingBox.bottomRight.getY())
+            return false;
+        
+        //If boxes aren't seperate, there is a collision
+        return true;
+        
+    }
+    
+}
---
+++ src/bresenhats/Level.java
@@ -0,0 +1,45 @@
+package bresenhats;
+
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+
+public class Level {
+    
+    private Image collisionLayer;
+    private Image graphicsLayer;
+    
+    private Vector2D startPosition;
+    
+    /*
+    Here is how all level files will be specified:
+    <world-number><level-number>c.png for the collisionLayer and
+    <world-number><level-number>g.png for the graphics layer.
+    
+    As an example level 1 of world 1 would be 11c.png and 11g.png
+    */
+    public Level(int _world, int _level, int _startX, int _startY){
+        String collisionFileName = "" + _world + _level + "c.png";
+        String graphicsFileName = "" + _world + _level + "g.png";
+        
+        this.collisionLayer = new Image(collisionFileName);
+        this.graphicsLayer = new Image(graphicsFileName);
+        
+        this.startPosition.setX(_startX);
+        this.startPosition.setY(_startY);
+    }
+    
+    public boolean isOverlapping(int x, int y){
+        if(x > 0 && x < this.collisionLayer.getWidth()){
+            if(y > 0 && y < this.collisionLayer.getHeight()){
+                return this.collisionLayer.getPixelReader().getColor(x, y).equals(Color.BLACK);
+            }
+        }
+        return true;
+    }
+    
+    
+    
+    
+    
+    
+}
