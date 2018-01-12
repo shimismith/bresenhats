59ab01db3773b832698c5696fcbca3400bf3ac3f
russellJacob <1bruss072@gmail.com>
2018-01-12 02:13:03 +0000

    Added classes to facilitate collisions

--- src/bresenhats/RigidBody.java
+++ src/bresenhats/RigidBody.java
@@ -34,19 +34,36 @@ public abstract class RigidBody extends MovableGameObject{
         return boundingBox.hasIntersected(secondBody.getBoundingBox());
     }
     
+    //Check if the rigidbody is colliding with the level, and stop it
+    public void handleLevelCollisions(Level lev){
+        if(this.willCollideWithLevelY(lev)){
+            if(this.getVel().getY() > 0){
+                this.setOnGround(true);
+            }
+            this.resolveCollisionsY(lev);
+        }
+        else{
+            this.setOnGround(false);
+        }
+        
+        if(this.willCollideWithLevelX(lev)){
+            this.resolveCollisionsX(lev);
+        }
+    }
+    
     /**
      * Checks if this rigid body has collided with the level
      * @param level
      * @return true iff this rigid body has collided
      */
-    public boolean hasCollidedWithLevel(Level level){
+    public boolean willCollideWithLevelX(Level level){
         
         //Loops iterate in a rectangle around object, checking for level intersections
         for(int yPos = this.getPosition().getYInt(); yPos <= this.getPosition().getYInt() + this.getHeight(); yPos++){
             for(int xPos = this.getPosition().getXInt(); xPos <= this.getPosition().getXInt() + this.getWidth(); xPos++){
 
                 //Check Collision
-                if(level.isOverlapping(xPos, yPos)){
+                if(level.isOverlapping(xPos + this.getVel().getXInt(), yPos)){
                     return true;
                 }
 
@@ -60,16 +77,55 @@ public abstract class RigidBody extends MovableGameObject{
         return false;
     }
     
-    public void addInstantaniousAcceleration(Vector2D acceleration){
-       // TODO complete
+    //Checks if the player will overlap with the level in the Y direction
+    public boolean willCollideWithLevelY(Level level){
+        
+        //Loop around rigidbody
+        for(int yPos = this.getPosition().getYInt(); yPos <= this.getPosition().getYInt() + this.getHeight(); yPos++){
+            for(int xPos = this.getPosition().getXInt(); xPos <= this.getPosition().getXInt() + this.getWidth(); xPos++){
+                
+                //Check Collision
+                if(level.isOverlapping(xPos, yPos + this.getVel().getYInt())){
+                    return true;
+                }
+                
+                if(yPos > this.getPosition().getYInt() && yPos < this.getPosition().getYInt() + this.getHeight() && xPos == this.getPosition().getXInt()){
+                    xPos = this.getPosition().getXInt() + this.getWidth() - 1;
+                }
+            }
+        }
+        return false;
     }
     
-    public void addConstantAcceleration(){
-      // TODO complete
+    //Stop the rigidbody from moving left or right
+    public void resolveCollisionsX(Level level){
+        this.setVelocity(new Vector2D(0, this.getVel().getY()));
+    }
+    
+    //Stop the rigidbody from moving up or down
+    public void resolveCollisionsY(Level level){
+        this.setVelocity(new Vector2D(this.getVel().getX(), 0));
+    }
+    
+    public void addInstantaniousAcceleration(Vector2D acceleration){
+       this.addVelocity(acceleration);
     }
     
     public void applyAllAccelerations(){
-        // TODO complete
+        this.addInstantaniousAcceleration(new Vector2D(-this.getVel().getX() * dragForceStrength, 0));
+    }
+    
+    public double getHorizontalVelocity(){
+        if(this.isOnGround()){
+            return horizontalVelocity;
+        }
+        else{
+            return horizontalAirVelocity;
+        }
+    }
+    
+    public double getVerticalVelocity(){
+        return verticalVelocity;
     }
     
     public AABB getBoundingBox(){
